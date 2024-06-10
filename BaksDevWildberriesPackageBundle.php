21ae96be437c4a554c3ac83281ae851f06fa84c4
kingstar <admin@baks.dev>
2024-06-10 10:49:45 +0300

    Baks Development

--- BaksDevWildberriesPackageBundle.php
+++ BaksDevWildberriesPackageBundle.php
@@ -35,4 +35,30 @@ class BaksDevWildberriesPackageBundle extends AbstractBundle
     public const NAMESPACE = __NAMESPACE__.'\\';
 
     public const PATH = __DIR__.DIRECTORY_SEPARATOR;
+
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $services = $container->services()
+            ->defaults()
+            ->autowire()
+            ->autoconfigure();
+
+        $services->load(self::NAMESPACE, self::PATH)
+            ->exclude([
+                self::PATH.'{Entity,Resources,Type}',
+                self::PATH.'**/*Message.php',
+                self::PATH.'**/*DTO.php',
+            ]);
+
+        $services->load(
+            self::NAMESPACE.'Type\Package\Status\WbPackageStatus\\',
+            self::PATH.'Type/Package/Status/WbPackageStatus'
+        );
+
+        $services->load(
+            self::NAMESPACE.'Type\Supply\Status\WbSupplyStatus\\',
+            self::PATH.'Type/Supply/Status/WbSupplyStatus'
+        );
+
+    }
 }
