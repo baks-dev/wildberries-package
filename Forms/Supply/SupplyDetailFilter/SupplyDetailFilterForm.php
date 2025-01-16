c3473e7e0f9dc17d9a4f97ca98f719d9ee1a08c0
kingstar <admin@baks.dev>
2025-01-16 16:36:25 +0300

    Baks Development

--- Forms/Supply/SupplyDetailFilter/SupplyDetailFilterForm.php
+++ Forms/Supply/SupplyDetailFilter/SupplyDetailFilterForm.php
@@ -91,7 +91,7 @@ final class SupplyDetailFilterForm extends AbstractType
                         return $category?->getValue();
                     },
                     'choice_label' => function(CategoryProductUid $category) {
-                        return ($category->getAttr() ? str_repeat('- &nbsp; &nbsp;', $category->getAttr() - 1) : '').$category->getOptions();
+                        return (is_int($category->getAttr()) ? str_repeat(' - ', $category->getAttr() - 1) : '').$category->getOptions();
                     },
                     'label' => false,
                     'required' => false,
