383a0b8d8eded1a96288fb05991e289d939aa59f
MichelAngelo <baksdevelopmen@gmail.com>
2025-11-12 23:34:37 +0300

    BaksDevelopment

--- Messenger/Orders/OrdersCompleteByCloseWbSupplyHandler.php
+++ Messenger/Orders/OrdersCompleteByCloseWbSupplyHandler.php
@@ -33,6 +33,7 @@ use BaksDev\Orders\Order\Entity\Event\OrderEvent;
 use BaksDev\Orders\Order\Entity\Order;
 use BaksDev\Orders\Order\Repository\CurrentOrderEvent\CurrentOrderEventInterface;
 use BaksDev\Orders\Order\Type\Id\OrderUid;
+use BaksDev\Orders\Order\Type\Status\OrderStatus\Collection\OrderStatusCompleted;
 use BaksDev\Orders\Order\Type\Status\OrderStatus\Collection\OrderStatusMarketplace;
 use BaksDev\Orders\Order\UseCase\Admin\Status\OrderStatusDTO;
 use BaksDev\Orders\Order\UseCase\Admin\Status\OrderStatusHandler;
@@ -130,7 +131,7 @@ final readonly class OrdersCompleteByCloseWbSupplyHandler
             }
 
             $OrderStatusDTO = new OrderStatusDTO(
-                OrderStatusMarketplace::class,
+                OrderStatusCompleted::class,
                 $OrderEvent->getId(),
             );
 
