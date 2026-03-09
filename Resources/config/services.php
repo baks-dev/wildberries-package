db3c54fbb6a7443e547cc4bc59f5d46939ac131b
MichelAngelo <baksdevelopmen@gmail.com>
2026-03-10 01:38:00 +0300

    BaksDevelopment

--- Resources/config/services.php
+++ Resources/config/services.php
@@ -30,8 +30,7 @@ return static function(ContainerConfigurator $configurator) {
     $services = $configurator->services()
         ->defaults()
         ->autowire()
-        ->autoconfigure()//->public()
-    ;
+        ->autoconfigure(); //->public();
 
     $NAMESPACE = BaksDevWildberriesPackageBundle::NAMESPACE;
     $PATH = BaksDevWildberriesPackageBundle::PATH;
